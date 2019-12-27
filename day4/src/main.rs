3ac4f3e55e74feefd7689a5368dc31dd6fef057d
Bob Somers <bob@bobsomers.com>
2019-12-26 21:15:29 -0800

    Day 4 complete.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -25,14 +25,29 @@ fn never_decreases_test() {
     assert!(!never_decreases(&[1, 2, 1, 2, 1, 2]));
 }
 
+fn not_part_of_larger_group(pass: &[i8; 6]) -> bool {
+    (pass[0] == pass[1] && pass[2] != pass[1])
+        || (pass[1] == pass[2] && pass[0] != pass[1] && pass[3] != pass[2])
+        || (pass[2] == pass[3] && pass[1] != pass[2] && pass[4] != pass[3])
+        || (pass[3] == pass[4] && pass[2] != pass[3] && pass[5] != pass[4])
+        || (pass[4] == pass[5] && pass[3] != pass[4])
+}
+
 #[test]
-fn both_conditions_test() {
-    let p = [1, 1, 1, 1, 1, 1];
-    assert!(has_same_adjacent_digits(&p) && never_decreases(&p));
-    let p = [2, 2, 3, 4, 5, 0];
-    assert!(!(has_same_adjacent_digits(&p) && never_decreases(&p)));
-    let p = [1, 2, 3, 7, 8, 9];
-    assert!(!(has_same_adjacent_digits(&p) && never_decreases(&p)));
+fn not_part_of_larger_group_test() {
+    assert!(not_part_of_larger_group(&[1, 1, 1, 1, 2, 2]));
+    assert!(not_part_of_larger_group(&[1, 1, 2, 2, 3, 3]));
+    assert!(!not_part_of_larger_group(&[1, 2, 3, 4, 4, 4]));
+}
+
+#[test]
+fn all_conditions_test() {
+    let p = [1, 1, 2, 2, 3, 3];
+    assert!(has_same_adjacent_digits(&p) && never_decreases(&p) && not_part_of_larger_group(&p));
+    let p = [1, 2, 3, 4, 4, 4];
+    assert!(!(has_same_adjacent_digits(&p) && never_decreases(&p) && not_part_of_larger_group(&p)));
+    let p = [1, 1, 1, 1, 2, 2];
+    assert!((has_same_adjacent_digits(&p) && never_decreases(&p) && not_part_of_larger_group(&p)));
 }
 
 fn main() {
@@ -46,7 +61,7 @@ fn main() {
             (i / 10 % 10) as i8,
             (i % 10) as i8
         ];
-        if has_same_adjacent_digits(&pass) && never_decreases(&pass) {
+        if has_same_adjacent_digits(&pass) && never_decreases(&pass) && not_part_of_larger_group(&pass) {
             eligible += 1;
         }
     }
